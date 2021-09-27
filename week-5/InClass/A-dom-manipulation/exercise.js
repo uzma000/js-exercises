5d692a239e0dbdb1b3decc1d9af8be1fabaf241f
uzma000 <uzmak3296@gmail.com>
2021-09-27 20:44:16 +0100

    done mandatory exercises
    done 1- 11

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -5,6 +5,8 @@ Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
 
+    
+
     2. the first div element node
     --> should log the ".site-header" node
 
@@ -15,15 +17,30 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+//task 1;
+// const para = document.querySelectorAll("p"); //getElementsbyTagName
+// console.log(para);
 
+// const heading = document.querySelector(".site-header"); //getElementsbyClassName
+// console.log(heading);
+
+// const text = document.getElementById("#jumbotron-text"); //querryselector
+// console.log(text);
+
+//const primary = document.querySelectorAll(".primary-content p"); //getElementbyClassName
+//console.log(primary);
+
+// let primary = document.getElementsByClassName("primary-content");
+// let allP = primary[0].getElementsByClassName("p");
+// console.log(allP);
 
 /*
 Task 2
 ======
 
+
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
 
 /*
 Task 3
@@ -32,7 +49,6 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-
 /*
 Task 4
 ======
@@ -40,11 +56,16 @@ Task 4
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
-
-
 /*
 Task 5
 ======
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+// const bigLink = document.querySelector("#largerLinksBtn");
+// bigLink.onClick = function () {
+//   const newButton = document.createElement("btn btn-secondary btn-lrg");
+//   bigLink.parentElement.appendChild(newButton);
+//   newButton.style.fontSize = "10px";
+// };
